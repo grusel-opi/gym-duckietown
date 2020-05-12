62b5889fd837c2ffc013cc560506757f1b18bad0
Felix Mayer <fmjmayer24@gmail.com>
2020-05-13 01:15:29 +0200

    improved image producer

--- image_producer.py
+++ image_producer.py
@@ -1,34 +1,47 @@
-import argparse
 import numpy as np
-import gym
+import os
 from gym_duckietown.envs import DuckietownEnv
-from matplotlib import pyplot as PLT
+from matplotlib import pyplot as plt
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--env-name', default=None)
-parser.add_argument('--map-name', default='udem1')
-parser.add_argument('--no-pause', action='store_true', help="don't pause on failure")
-args = parser.parse_args()
 
-if args.env_name is None:
+def get_images():
     env = DuckietownEnv(
-        map_name=args.map_name,
-        domain_rand=False,
+        map_name='udem1',
+        domain_rand=True,
         draw_bbox=False,
-        full_transparency=True
+        full_transparency=True,
+        accept_start_angle_deg=360,
     )
-else:
-    env = gym.make(args.env_name)
 
-num_imgs = 1
-imgs = list()
+    images = list()
 
-for i in range(num_imgs):
-    start = list(env.drivable_tiles[int(np.random.uniform(0, len(env.drivable_tiles)))]['coords'])
-    env.user_tile_start = start
-    env.reset()
-    lane_pos = env.get_agent_info()['Simulator']['lane_position']
-    label = (lane_pos.dist, lane_pos.dot_dir)
-    imgs.append((env.render(mode='rgb_array'), label))
+    for _ in range(num_imgs):
+        start = list(env.drivable_tiles[int(np.random.uniform(0, len(env.drivable_tiles)))]['coords'])
+        env.user_tile_start = start
+        env.reset()
+        dist = env.get_agent_info()['Simulator']['lane_position']['dist']
+        dot_dir = env.get_agent_info()['Simulator']['lane_position']['dot_dir']
+        images.append((env.render(mode='rgb_array'), (dist, dot_dir)))
+
+    return images
 
 
+if __name__ == '__main__':
+
+    save = True
+    num_imgs = 100
+
+    imgs = get_images()
+
+    if save:
+
+        path = "./pictures/"
+
+        try:
+            os.mkdir(path)
+        except OSError:
+            print("./pictures may already be there..")
+            exit(-1)
+
+        for img, label in imgs:
+            plt.imsave(path + str(label) + ".png", img)

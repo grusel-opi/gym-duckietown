c59bb7ef53aca6aaa3d999847560201a12d05769
Stephan Perren <stephan.perren@htwg-konstanz.de>
2020-05-23 14:17:27 +0200

    The DataLoader now supports batches of data. The batches can also be visualized.

--- neural-perception/data/data_loader.py
+++ neural-perception/data/data_loader.py
@@ -8,14 +8,34 @@ from matplotlib import pyplot as plt
 class DataLoader:
     def __init__(self, config):
         self.config = config
-        set_size = len(fnmatch.filter(os.listdir(config.data_dir), "data*"))
+        self.set_size = len(fnmatch.filter(os.listdir(config.data_dir), "data*"))
         self.data = []
 
-        for set_no in range(0, set_size):
+        for set_no in range(self.set_size):
             set = load(config.data_dir + "/data" + str(set_no) + ".npy")
             labels = load(config.data_dir + "/labels" + str(set_no) + ".npy")
             self.data.append((set, labels))
 
     def next_batch(self, batch_size):
-        idx = np.random.choice(500, batch_size)
-        yield self.input[idx], self.y[idx]
+        batch = []
+
+        for n in range(batch_size):
+            set_idx = np.random.choice(self.set_size)
+            set_imgs = self.data[set_idx][0]
+            set_labels = self.data[set_idx][1]
+            sample_idx = np.random.choice(len(set_imgs))
+
+            batch.append((set_imgs[sample_idx], set_labels[sample_idx]))
+
+        return batch
+
+    def show_batch(self, batch):
+        plt.figure(figsize=(50, 50))
+
+        for n in range(len(batch)):
+            ax = plt.subplot(5, 5, n + 1)
+            plt.imshow(batch[n][0])
+            plt.title(batch[n][1], fontdict = {'fontsize' : 50})
+            plt.axis('off')
+
+        plt.show()

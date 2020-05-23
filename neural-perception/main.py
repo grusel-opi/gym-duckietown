859a83929e10a60fb603392cb0fa330dce6bac3d
Stephan Perren <stephan.perren@htwg-konstanz.de>
2020-05-23 15:49:55 +0200

    Updated the main routine to work with TF v2.

--- neural-perception/main.py
+++ neural-perception/main.py
@@ -1,20 +1,18 @@
 import tensorflow as tf
-import sys
 
-sys.path.append('.')
-
-from .data.data_loader import DataLoader
-from .model.model import PoseRegress
-from .train.train import Train
-from .util.config import process_config
-from .util.dirs import create_dirs
-from .util.logger import Logger
-from .util.args import get_args
+from data.data_loader import DataLoader
+from model.model import PoseRegress
+from trainer.trainer import Trainer
+from util.config import process_config
+from util.dirs import create_dirs
+from util.logger import Logger
+from util.args import get_args
 
 
 def main():
-    # capture the config path from the run arguments
-    # then process the json configuration file
+    print("TensorFlow version: {}".format(tf.__version__))
+    print("Eager execution: {}".format(tf.executing_eagerly()))
+
     try:
         args = get_args()
         config = process_config(args.config)
@@ -22,23 +20,19 @@ def main():
         print("missing or invalid arguments")
         exit(0)
 
-    # create the experiments dirs
     create_dirs([config.summary_dir, config.checkpoint_dir])
-    # create tensorflow session
-    sess = tf.Session()
-
     data_loader = DataLoader(config)
-    print(data_loader.data)
-
-    # create an instance of the model you want
     model = PoseRegress(config)
-    # create tensorboard logger
-    logger = Logger(sess, config)
-    # create trainer and pass all the previous components to it
-    trainer = Train(sess, model, data_loader, config, logger)
-    # load model if exists
-    model.load(sess)
-    # here you train your model
+
+    # TODO: integrate the Logger
+    # logger = Logger(config)
+
+    # TODO: update the logger in the trainer
+    trainer = Trainer(model, data_loader, config, logger=None)
+
+    # TODO: Support the model to save and load
+    # model.load(sess)
+
     trainer.train()
 
 

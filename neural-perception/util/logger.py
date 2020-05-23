2b4ad7139feeaa2eb104a667833011e872e8bf37
Stephan Perren <stephan.perren@htwg-konstanz.de>
2020-05-23 15:53:43 +0200

    Added TODO.

--- neural-perception/util/logger.py
+++ neural-perception/util/logger.py
@@ -1,27 +1,27 @@
 import tensorflow as tf
 import os
 
+# TODO: We have to remove the Session so that we can use the Logger for TF v2.
 
 class Logger:
-    def __init__(self, sess, config):
-        self.sess = sess
+    def __init__(self, config):
         self.config = config
         self.summary_placeholders = {}
         self.summary_ops = {}
-        self.train_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "train"),
+        self.train_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "trainer"),
                                                           self.sess.graph)
         self.test_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "test"))
 
     # it can summarize scalars and images.
-    def summarize(self, step, summarizer="train", scope="", summaries_dict=None):
+    def summarize(self, step, summarizer="trainer", scope="", summaries_dict=None):
         """
         :param step: the step of the summary
-        :param summarizer: use the train summary writer or the test one
+        :param summarizer: use the trainer summary writer or the test one
         :param scope: variable scope
         :param summaries_dict: the dict of the summaries values (tag,value)
         :return:
         """
-        summary_writer = self.train_summary_writer if summarizer == "train" else self.test_summary_writer
+        summary_writer = self.train_summary_writer if summarizer == "trainer" else self.test_summary_writer
         with tf.variable_scope(scope):
 
             if summaries_dict is not None:

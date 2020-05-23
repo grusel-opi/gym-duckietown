7867bbc95ab5f2df0704678c3f62adf383e0783b
Stephan Perren <stephan.perren@htwg-konstanz.de>
2020-05-23 15:51:41 +0200

    Updated the trainer to work with TF v2.

--- neural-perception/trainer/trainer.py
+++ neural-perception/trainer/trainer.py
@@ -1,44 +1,55 @@
-import tensorflow as tf
 from tqdm import tqdm
 import numpy as np
 
-class Train:
-    def __init__(self, sess, model, data, config, logger):
+
+class Trainer:
+    def __init__(self, model, data_loader, config, logger):
         self.model = model
         self.logger = logger
         self.config = config
-        self.sess = sess
-        self.data = data
-        self.init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-        self.sess.run(self.init)
+        self.data_loader = data_loader
 
     def train(self):
-        for cur_epoch in range(self.model.cur_epoch_tensor.eval(self.sess), self.config.num_epochs + 1, 1):
+        for epoch in range(self.config.num_epochs):
             self.train_epoch()
-            self.sess.run(self.model.increment_cur_epoch_tensor)
 
     def train_epoch(self):
-        loop = tqdm(range(self.config.num_iter_per_epoch))
+        loop = tqdm(range(self.config.num_episodes))
         losses = []
         accs = []
+
         for _ in loop:
             loss, acc = self.train_step()
             losses.append(loss)
             accs.append(acc)
+
         loss = np.mean(losses)
         acc = np.mean(accs)
 
-        cur_it = self.model.global_step_tensor.eval(self.sess)
-        summaries_dict = {
-            'loss': loss,
-            'acc': acc,
-        }
-        self.logger.summarize(cur_it, summaries_dict=summaries_dict)
-        self.model.save(self.sess)
+        # TODO: configure logger,
+        #       save summaries,
+        #       save model
+
+        # cur_it = self.model.global_step_tensor.eval(self.sess)
+        # summaries_dict = {
+        #     'loss': loss,
+        #     'acc': acc,
+        # }
+        # self.logger.summarize(cur_it, summaries_dict=summaries_dict)
+        # self.model.save(self.sess)
 
     def train_step(self):
-        batch_x, batch_y = next(self.data.next_batch(self.config.batch_size))
-        feed_dict = {self.model.x: batch_x, self.model.y: batch_y, self.model.is_training: True}
-        _, loss, acc = self.sess.run([self.model.train_step, self.model.cross_entropy, self.model.accuracy],
-                                     feed_dict=feed_dict)
+        batch_imgs, batch_labels = self.data_loader.next_batch(self.config.batch_size)
+        # self.data_loader.show_batch(batch_imgs, batch_labels)
+
+        norm_batch_imgs = batch_imgs / 255.
+        norm_batch_imgs = norm_batch_imgs[:1]
+
+        d, a = self.model.call(norm_batch_imgs[:2])
+        # print("d: %s, a: %s" % (str(d.numpy()[0][0]), str(a.numpy()[0][0])))
+
+        # TODO: calculate loss and acc
+        loss = None
+        acc = None
+
         return loss, acc

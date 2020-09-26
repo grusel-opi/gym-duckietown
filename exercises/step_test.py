5f96b9632de4f508350d3063ce9714ae345150c1
Jan296929B <jan@berchtold-web.com>
2020-09-26 13:52:00 +0200

    added step_test.py

---
+++ exercises/step_test.py
@@ -0,0 +1,78 @@
+# !/usr/bin/env python3
+"""
+Simple exercise to construct a controller that controls the simulated Duckiebot using pose.
+"""
+
+import time
+import sys
+import argparse
+import math
+import numpy as np
+import gym
+import functools
+from gym_duckietown.envs import DuckietownEnv
+from lokalisierung.MCL import MCL
+
+from lokalisierung.Particle import Particle
+from lokalisierung.Ducky_map import DuckieMap
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env-name', default=None)
+parser.add_argument('--map-name', default='udem1')
+parser.add_argument('--no-pause', action='store_true', help="don't pause on failure")
+args = parser.parse_args()
+
+if args.env_name is None:
+    env = DuckietownEnv(
+        map_name=args.map_name,
+        domain_rand=False,
+        draw_bbox=False
+    )
+else:
+    env = gym.make(args.env_name)
+
+obs = env.reset()
+env.render()
+
+total_reward = 0
+px, _, py = env.cur_pos
+my_map = DuckieMap("../gym_duckietown/maps/udem1.yaml")
+aParticle = Particle(px, py, 1, 'p1', my_map, env,angle=env.cur_angle)
+aParticle.set_tile()
+print('start particle position und angle', aParticle.p_x, aParticle.p_y, aParticle.angle)
+step_counter = 0
+while True:
+    lane_pose = env.get_lane_pos2(env.cur_pos, env.cur_angle)
+    distance_to_road_center = lane_pose.dist
+    angle_from_straight_in_rads = lane_pose.angle_rad
+
+    # Start changing the code here.
+    k_p = 10
+    k_d = 1
+
+    # The speed is a value between [0, 1] (which corresponds to a real speed between 0m/s and 1.2m/s)
+
+    speed = 0.2  # TODO: You should overwrite this value
+
+    # angle of the steering wheel, which corresponds to the angular velocity in rad/s
+    steering = k_p * distance_to_road_center + k_d * angle_from_straight_in_rads  # TODO: You should overwrite this value
+
+    # No need to edit code below.
+    obs, reward, done, info = env.step([speed, steering])
+    aParticle.step([speed, steering])
+    print('pos_particle and angle', [aParticle.p_x,0.0 ,aParticle.p_y], aParticle.angle)
+    print('pos_robot and angle', env.cur_pos, env.cur_angle)
+    step_counter += 1
+
+    total_reward += reward
+
+    print('Steps = %s, Timestep Reward=%.3f, Total Reward=%.3f' % (env.step_count, reward, total_reward))
+    print('Distance to road center: ', distance_to_road_center)
+
+    env.render()
+
+    if done:
+        if reward < 0:
+            print('*** CRASHED ***')
+        print('Final Reward = %.3f' % total_reward)
+        break

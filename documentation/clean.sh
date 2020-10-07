f70ad5dfdc9ba27b1a123df8507b2c769f270863
Stephan Perren <stephan.perren@htwg-konstanz.de>
2020-10-07 16:03:50 +0200

    Added documentation

---
+++ documentation/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+find . -name "*.aux" -print0 | xargs -0 rm
+find . -maxdepth 1 -name "*.bbl" -print0 | xargs -0 rm 
+find . -maxdepth 1 -name "*.blg" -print0 | xargs -0 rm 
+find . -maxdepth 1 -name "*.lof" -print0 | xargs -0 rm 
+find . -maxdepth 1 -name "*.log" -print0 | xargs -0 rm 
+find . -maxdepth 1 -name "*.out" -print0 | xargs -0 rm 
+find . -maxdepth 1 -name "*.synctex.gz" -print0 | xargs -0 rm 
+find . -maxdepth 1 -name "*.toc" -print0 | xargs -0 rm 
+find . -maxdepth 1 -name "*.pdf" -print0 | xargs -0 rm

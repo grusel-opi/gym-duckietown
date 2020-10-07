9b209948c85b9a59036e0da87faa9b1acd4343de
Stephan Perren <stephan.perren@htwg-konstanz.de>
2020-10-07 15:05:21 +0200

    PID controller is now working for tf modell as well

--- neural_perception/simulator/model_test.py
+++ neural_perception/simulator/model_test.py
@@ -9,8 +9,7 @@ from neural_perception.util.pid_controller import PID, calculate_out_lim
 from neural_perception.util.util import preprocess, get_lane_pos
 import tensorflow as tf
 
-DEBUG = False
-MANUAL_CONTROL = False
+
 
 env = DuckietownEnv(domain_rand=False,
                     draw_bbox=False)
@@ -39,35 +38,43 @@ def on_key_press(symbol, modifiers):
             print("Manual control activated")
         else:
             print("Manual control deactivated")
+            pid.reset()
         MANUAL_CONTROL = not MANUAL_CONTROL
 
 
 key_handler = key.KeyStateHandler()
 env.unwrapped.window.push_handlers(key_handler)
 
+DEBUG = False
+MANUAL_CONTROL = False
 model = tf.keras.models.load_model('saved_model/17.09.2020-13:40:11')
 speed = 0.3
 target = 25
-k_u = 0.125
-p_u = 20
-k_p = 0.6 * k_u
-k_i = 2 * k_p / p_u
-k_d = k_p * p_u / 8
 
-pid = PID(k_p, k_i, k_d, target)
+k_p_cheat = 0.075
+k_i_cheat = 0.0075
+k_d_cheat = 0.1875
+
+k_p_modell = 0.075
+k_i_modell = 0.00075
+k_d_modell = 0.000075
+
+pid = PID(k_p_modell, k_i_modell, k_d_modell, target)
 out_lim = calculate_out_lim(env, speed)
 pid.set_out_lim(out_lim)
+correction = 0
 
 def update(dt):
-    global speed
     global obs
+    global correction
 
     lane_pos = get_lane_pos(env)
     dist_to_road_edge = lane_pos.dist_to_edge
-    # pred_dist = model.predict(obs)[0][0]
+    pred_dist = model.predict(obs)[0][0]
 
     if not MANUAL_CONTROL:
-        correction = pid.update(dist_to_road_edge * 100)
+        # correction = pid.update(dist_to_road_edge * 100)
+        correction = pid.update(pred_dist)
         action = np.array([speed, correction])
     else:
         action = np.array([0.0, 0.0])
@@ -83,14 +90,14 @@ def update(dt):
 
     if DEBUG:
         print()
-        # print("pred_dist: ", pred_dist)
-        # print("dist_to_road_edge: ", dist_to_road_edge)
+        print("pred_dist: ", pred_dist)
+        print("dist_to_road_edge: ", dist_to_road_edge)
         print("correction: ", correction)
         # print("out_lim: ", out_lim)
         # print("signed_dist: ", lane_pos.dist)
         # print("dot_dir: ", lane_pos.dot_dir)
         # print("angle_deg: ", lane_pos.angle_deg)
-        # print("dist_err: ", abs(dist_to_road_edge * 100 - pred_dist))
+        print("dist_err: ", abs(dist_to_road_edge * 100 - pred_dist))
 
     obs, _, _, _ = env.step(action)
     obs = preprocess(obs)

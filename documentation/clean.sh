a96c018d99b9583f462458db43693eced2333e83
Stephan Perren <stephan.perren@htwg-konstanz.de>
2020-10-08 13:02:58 +0200

    Updated clean.sh

--- documentation/clean.sh
+++ documentation/clean.sh
@@ -9,3 +9,4 @@ find . -maxdepth 1 -name "*.out" -print0 | xargs -0 rm
 find . -maxdepth 1 -name "*.synctex.gz" -print0 | xargs -0 rm 
 find . -maxdepth 1 -name "*.toc" -print0 | xargs -0 rm 
 find . -maxdepth 1 -name "*.pdf" -print0 | xargs -0 rm
+find . -maxdepth 1 -name "*.lol" -print0 | xargs -0 rm
